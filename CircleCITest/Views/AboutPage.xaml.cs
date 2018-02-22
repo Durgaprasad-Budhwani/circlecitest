7f5943b74f5637386694a73934d303d403f44965
durgaprasad <durgaprasad.budhwani@gmail.com>
2018-02-22 13:21:21 +0530

    added test branch

---
+++ CircleCITest/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CircleCITest
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
